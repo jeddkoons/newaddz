ddbbea6aad8ef0fac21f94a4dea23dd484375b40
“jeddkoons” <jeddkoons@gmail.comssh-keygen -t rsa -C jeddkoons@gmail.com$ git config --global color.ui truegit config --global user.name jeddkoonsgit config --global user.email jeddkoons@gmail.com>
2016-03-03 10:41:45 +0100

    hello world loop

--- app.py
+++ app.py
@@ -15,17 +15,20 @@ RUN_EVERY_N_SECONDS = 60*15 # e.g. 60*5 = tweets every five minutes
 
 
 naughty_words = [" -RT"]
-good_words = ["vyvanse got me like", "adderall got me like", "adderall had me like", "TFW vyvanse"]
+good_words = ["TFW vyvanse", "TFW adderall", "adderall got me like", "vyvanse got me like", "adderall had me like", "TFW vyvanse"]
 filter = " OR ".join(good_words)
 blacklist = " -".join(naughty_words)
 keywords = filter + blacklist
 
 twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
 
+
+
 #The obligatory first status update to test
 twitter.update_status(status="Hello world.")
 
-search_results = twitter.search(q=keywords, count=100)
+
+search_results = twitter.search(q=keywords, result_type="mixed", count=100)
 try:
     for tweet in search_results["statuses"]:
         try:
@@ -37,7 +40,6 @@ except TwythonError as e:
     print e
     
   
-        # random_favoriting(['apples', 'oranges'], handle)
 time.sleep(RUN_EVERY_N_SECONDS)
 
 if __name__ == '__main__':
