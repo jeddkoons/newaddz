9cfabdddff9033e510e8cde57b5d9c1d808f4a60
“jeddkoons” <jeddkoons@gmail.comssh-keygen -t rsa -C jeddkoons@gmail.com$ git config --global color.ui truegit config --global user.name jeddkoonsgit config --global user.email jeddkoons@gmail.com>
2016-03-03 09:03:11 +0100

    maybe a fuckup

--- app.py
+++ app.py
@@ -1,69 +1,44 @@
 import os
 import time
 from random import random
-from twython import Twython
+from twython import Twython, TwythonError
 
-CONSUMER_KEY = os.environ['TWITTER_CONSUMER_KEY']
-CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
-OAUTH_TOKEN = os.environ['TWITTER_OAUTH_TOKEN']
-OAUTH_TOKEN_SECRET = os.environ['TWITTER_OAUTH_TOKEN_SECRET']
+app_key = "0s24wXPrr5Qe6enob5Xyn7VyZ"
+app_secret = "jI0JY8RQMv7rylZnzdzIK6bLNcPGiPf8u4d7Fq0oz9PEAIdc92"
+oauth_token = "704999209636864000-9kbaJ8hms0BdI0aF2vdA2JE24XXNOiv"
+oauth_token_secret = "wjEqiZLI2ieu5b9ydWrNeiWKupJcrWxJ9n8jDYVvoUYpe"
 TWEET_LENGTH = 140
 TWEET_URL_LENGTH = 21
 
-RUN_EVERY_N_SECONDS = 60*5 # e.g. 60*5 = tweets every five minutes
+RUN_EVERY_N_SECONDS = 60*15 # e.g. 60*5 = tweets every five minutes
 
-USERS_TO_IGNORE = []
-DO_NOT_FAVORITE_USERS_AGAIN = True
 
-def twitter_handle():
-    return Twython(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-def favorite_tweet(tweet, handle):
-    handle.create_favorite(id=tweet['id'])
+naughty_words = [" -RT"]
+good_words = ["vyvanse got me like", "adderall got me like", "adderall had me like", "TFW vyvanse"]
+filter = " OR ".join(good_words)
+blacklist = " -".join(naughty_words)
+keywords = filter + blacklist
 
-def random_favoriting(keywords, handle, favorite_probability=0.2):
-    """
-    keywords is a list, like ['bananas', 'apples', 'oranges']
+twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
 
-    n.b. if this function is called every N seconds
-        then you can expect to favorite a tweet
-        once every N/favorite_probability seconds
-    """
-    for keyword in keywords:
-        xs = handle.search(q=keyword)
-        ts = [x for x in xs['statuses']]
-        if DO_NOT_FAVORITE_USERS_AGAIN:
-            ts = [x for x in ts if x['user']['id'] not in USERS_TO_IGNORE]
-        if ts:
-            if random() < favorite_probability: 
-                print 'Favoriting: ' + ts[0]['text']
-                favorite_tweet(ts[0], handle)
-                if DO_NOT_FAVORITE_USERS_AGAIN:
-                    USERS_TO_IGNORE.append(ts[0]['user']['id'])
-                return
+#The obligatory first status update to test
+twitter.update_status(status="Hello world.")
 
-def submit_tweet(message, handle=None):
-    if not handle:
-        handle = twitter_handle()
-    handle.update_status(status=message)
-
-def get_message(handle):
-    """
-    Your code goes here!
-    """
-    message = 'I TWEET THIS.'
-    assert len(message) <= TWEET_LENGTH
-    return message
-
-def main():
-    handle = twitter_handle()
-    USERS_TO_IGNORE.extend([x['user']['id'] for x in handle.get_favorites()])
-    while True:
-        message = get_message(handle)
-        print message
-        submit_tweet(message, handle)
+search_results = twitter.search(q=keywords, count=100)
+try:
+    for tweet in search_results["statuses"]:
+        try:
+            twitter.retweet(id = tweet["id_str"])
+        except TwythonError as e:
+            print e
+           
+except TwythonError as e:
+    print e
+    
+  
         # random_favoriting(['apples', 'oranges'], handle)
-        time.sleep(RUN_EVERY_N_SECONDS)
+time.sleep(RUN_EVERY_N_SECONDS)
 
 if __name__ == '__main__':
     main()
