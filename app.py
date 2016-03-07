2e647fd158e1273403d64f972a4b62862686ee6d
“jeddkoons” <jeddkoons@gmail.comssh-keygen -t rsa -C jeddkoons@gmail.com$ git config --global color.ui truegit config --global user.name jeddkoonsgit config --global user.email jeddkoons@gmail.com>
2016-03-07 10:38:05 +0100

    added quotes

--- app.py
+++ app.py
@@ -7,22 +7,21 @@ app_key = "0s24wXPrr5Qe6enob5Xyn7VyZ"
 app_secret = "jI0JY8RQMv7rylZnzdzIK6bLNcPGiPf8u4d7Fq0oz9PEAIdc92"
 oauth_token = "704999209636864000-9kbaJ8hms0BdI0aF2vdA2JE24XXNOiv"
 oauth_token_secret = "wjEqiZLI2ieu5b9ydWrNeiWKupJcrWxJ9n8jDYVvoUYpe"
-TWEET_LENGTH = 140
-TWEET_URL_LENGTH = 21
 
-RUN_EVERY_N_SECONDS = 60*15 # e.g. 60*5 = tweets every five minutes
+
+RUN_EVERY_N_SECONDS = 60*30 # e.g. 60*5 = tweets every five minutes
 
 
 
 naughty_words = [" -RT"]
-good_words = ["TFW vyvanse", "TFW adderall", "adderall got me like", "vyvanse got me like", "adderall had me like", "TFW vyvanse"]
+good_words = ["%22adderall got me like%22", "%22vyvanse got me like%22", "%22vyvanse had me like%22", "%22adderall had me like%22", "%22TFW vyvanse%22", "%22TFW adderall%22"]
 filter = " OR ".join(good_words)
 blacklist = " -".join(naughty_words)
 keywords = filter + blacklist
 
 twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
 
-search_results = twitter.search(q=keywords, result_type="mixed", count=100)
+search_results = twitter.search(q=keywords, count=10)
 try:
     for tweet in search_results["statuses"]:
         try:
